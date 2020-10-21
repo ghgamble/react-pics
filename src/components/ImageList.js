62a84f0584f4b5ed920ace14e29b21443f396968
gracehg <grace@statusforward.com>
2020-10-21 11:12:35 -0400

    finished project with image card and css

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,13 @@
+import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
 
-const ImageList = (props) => {
-      const images = props.images.map(({ alt_description, id, urls }) => {
-            return <img key={id} src={urls.regular} alt={alt_description} />
+const ImageList = props => {
+      const images = props.images.map(image => {
+            return <ImageCard key={image.id} image={image} />
       });
       return (
-            <div>{images}</div>
+            <div className="image-list" >{images}</div>
       );
 }
 
